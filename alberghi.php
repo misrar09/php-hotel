edf271b9e2908864bc8b681cda28bcfcbb5175a8
israr <misrar09@gmail.com>
2023-12-19 08:03:50 +0100

    print all hotels info using for loop

--- alberghi.php
+++ alberghi.php
@@ -4,10 +4,70 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotels php</title>
+
+    <?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
 </head>
 <body>
 
-<?php ?>
+<?php for ($i = 0; $i < count($hotels); $i++) { ?>
+    <ul>
+        <li>Name:<?php echo $hotels[$i]["name"]; ?></li>
+        <li>Description: <?php echo $hotels[$i]["description"]; ?></li>
+        <li>Parking: <?php echo $hotels[$i]["parking"]; ?></li>
+        <li>Vote: <?php echo $hotels[$i]["vote"]; ?></li>
+        <li>Distance to Center: <?php echo $hotels[$i]["distance_to_center"]; ?></li>
+
+    </ul>
+<?php } ?>
+
+
+
+
+
+
+
     
 </body>
 </html>
